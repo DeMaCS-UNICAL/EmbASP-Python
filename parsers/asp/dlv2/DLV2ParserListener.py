25b55a31ff782e55c7981f1eb83d964216564c3a
fpacenza <francescopacenza93@gmail.com>
2022-04-07 18:28:21 +0200

    updated dlv2 parser and lexer

---
+++ parsers/asp/dlv2/DLV2ParserListener.py
@@ -0,0 +1,75 @@
+# Generated from /home/francesco/Scrivania/embasp/EmbASP-antlr-grammars/dlv2/DLV2Parser.g4 by ANTLR 4.9.2
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .DLV2Parser import DLV2Parser
+else:
+    from DLV2Parser import DLV2Parser
+
+# This class defines a complete listener for a parse tree produced by DLV2Parser.
+class DLV2ParserListener(ParseTreeListener):
+
+    # Enter a parse tree produced by DLV2Parser#answer_set.
+    def enterAnswer_set(self, ctx:DLV2Parser.Answer_setContext):
+        pass
+
+    # Exit a parse tree produced by DLV2Parser#answer_set.
+    def exitAnswer_set(self, ctx:DLV2Parser.Answer_setContext):
+        pass
+
+
+    # Enter a parse tree produced by DLV2Parser#cost.
+    def enterCost(self, ctx:DLV2Parser.CostContext):
+        pass
+
+    # Exit a parse tree produced by DLV2Parser#cost.
+    def exitCost(self, ctx:DLV2Parser.CostContext):
+        pass
+
+
+    # Enter a parse tree produced by DLV2Parser#level.
+    def enterLevel(self, ctx:DLV2Parser.LevelContext):
+        pass
+
+    # Exit a parse tree produced by DLV2Parser#level.
+    def exitLevel(self, ctx:DLV2Parser.LevelContext):
+        pass
+
+
+    # Enter a parse tree produced by DLV2Parser#model.
+    def enterModel(self, ctx:DLV2Parser.ModelContext):
+        pass
+
+    # Exit a parse tree produced by DLV2Parser#model.
+    def exitModel(self, ctx:DLV2Parser.ModelContext):
+        pass
+
+
+    # Enter a parse tree produced by DLV2Parser#output.
+    def enterOutput(self, ctx:DLV2Parser.OutputContext):
+        pass
+
+    # Exit a parse tree produced by DLV2Parser#output.
+    def exitOutput(self, ctx:DLV2Parser.OutputContext):
+        pass
+
+
+    # Enter a parse tree produced by DLV2Parser#predicate_atom.
+    def enterPredicate_atom(self, ctx:DLV2Parser.Predicate_atomContext):
+        pass
+
+    # Exit a parse tree produced by DLV2Parser#predicate_atom.
+    def exitPredicate_atom(self, ctx:DLV2Parser.Predicate_atomContext):
+        pass
+
+
+    # Enter a parse tree produced by DLV2Parser#term.
+    def enterTerm(self, ctx:DLV2Parser.TermContext):
+        pass
+
+    # Exit a parse tree produced by DLV2Parser#term.
+    def exitTerm(self, ctx:DLV2Parser.TermContext):
+        pass
+
+
+
+del DLV2Parser
