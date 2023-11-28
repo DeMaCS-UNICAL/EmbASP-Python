c602e9e688084d700a8c6fd3adfacbd2e25548ef
Marco Duca <dcumrc02a07d086r@studenti.unical.it>
2023-11-28 19:51:37 +0100

    Added __init__.py to embasp folder

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='EmbASP',
-      version='8.0.0',
+      version='8.1.1',
       description='EmbASP',
       long_description=open('README.md').read(),
       author='Department of Mathematics and Computer Science, University of Calabria',
